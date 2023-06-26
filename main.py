88dc636955e82ea4066c9061b29b33513006d76f
Vishal Mohan <39985061+vishalm30@users.noreply.github.com>
2023-06-26 12:38:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -9,21 +9,5 @@ input_video_path = 'test.gif'
 video = cv2.VideoCapture(input_video_path)
 
 
-# face_detection(video)
-# audio_detection(input_video_path)
-
-
-if __name__ == '__main__':
-    # Create separate processes for each function
-    process1 = multiprocessing.Process(target=face_detection(video))
-    process2 = multiprocessing.Process(target=audio_detection(input_video_path))
-
-    # Start both processes
-    process1.start()
-    process2.start()
-
-    # Wait for both processes to finish
-    process1.join()
-    process2.join()
-
-    print("Both functions completed")
+face_detection(video)
+audio_detection(input_video_path)
